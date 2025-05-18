7449d5d24aa6ae684badb4fe1803f217b10618af
marklileo <phornphatmak1112@gmail.com>
2025-05-18 20:56:20 +0700

    fix: Add suspense of useSearchParams() + Expand skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import SearchBar from "./components/searchBar";
 import PokemonResult from "./components/pokemonResult";
+import { Suspense } from "react";
 
 const Home = () => {
   return (
-    <div className="min-h-screen gap-2 bg-gradient-to-b from-gray-100 to-gray-300 p-6 flex flex-col items-center">
-      <h1 className="text-4xl font-bold mb-6">Pokémon Search</h1>
-      <SearchBar />
-      <PokemonResult />
-    </div>
+    <Suspense>
+      <div className="min-h-screen gap-2 bg-gradient-to-b from-gray-100 to-gray-300 p-6 flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-6">Pokémon Search</h1>
+        <SearchBar />
+        <PokemonResult />
+      </div>
+    </Suspense>
   );
 };
 
