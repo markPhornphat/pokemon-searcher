20a75c4c52f27f949060ea8365d6d4159835325b
marklileo <phornphatmak1112@gmail.com>
2025-05-19 21:41:41 +0700

    test: Auto test with jest

--- app/app.test.tsx
+++ app/app.test.tsx
@@ -1,6 +1,17 @@
 import "@testing-library/jest-dom";
-import { render, screen, cleanup } from "@testing-library/react";
+import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
+import {
+  render,
+  screen,
+  cleanup,
+  fireEvent,
+  waitFor,
+} from "@testing-library/react";
 import SearchBar from "./components/searchBar";
+import PokemonResult from "./components/pokemonResult";
+import Card from "./components/ui/card";
+import fetch from "cross-fetch";
+global.fetch = fetch; //Allow ApolloClient to fetch
 
 // Mock the App Router Hooks
 jest.mock("next/navigation", () => ({
@@ -12,6 +23,16 @@ jest.mock("next/navigation", () => ({
   }),
 }));
 
+//Mocking the calling graphQL
+const client = new ApolloClient({
+  uri: "https://graphql-pokemon2.vercel.app/",
+  cache: new InMemoryCache(),
+});
+
+function renderWithApollo(ui: React.ReactElement) {
+  return render(<ApolloProvider client={client}>{ui}</ApolloProvider>);
+}
+
 afterEach(() => {
   cleanup();
 });
@@ -21,8 +42,47 @@ describe("Search component", () => {
     render(<SearchBar />);
   });
 
-  test("Button Rendering", () => {
+  test("Search Button Rendering", () => {
     const searchButton = screen.getByTestId("search-button");
     expect(searchButton).toBeInTheDocument();
+  });
+
+  test("Search field Rendering", () => {
+    const searchField = screen.getByTestId("search-field");
+    expect(searchField).toBeInTheDocument();
+  });
+});
+
+describe("Input pokemon in search field", () => {
+  beforeEach(() => {
+    render(<SearchBar />);
+    renderWithApollo(<PokemonResult />);
+  });
+
+  //To test each Pokemon with their type
+  test.each([
+    {
+      name: "Bulbasaur",
+      types: ["Poison", "Grass"],
+    },
+    {
+      name: "Charmander",
+      types: ["Fire"],
+    },
+    {
+      name: "Squirtle",
+      types: ["Water"],
+    },
+  ])("Input %s and see result", async ({ name, types }) => {
+    const input = screen.getByTestId("search-field");
+    const searchButton = screen.getByTestId("search-button");
+
+    fireEvent.change(input, { target: { value: name } });
+    fireEvent.click(searchButton);
+
+    for (const type of types) {
+      const typeElements = await screen.findAllByText(type);
+      expect(typeElements.length).toBeGreaterThan(0);
+    }
   });
 });
